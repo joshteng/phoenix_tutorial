4acc734c3d0eb828eb6ff0733c7b3c45d0d66a29
Josh Teng <joshteng@me.com>
2020-05-18 02:12:00 +0800

    done

---
+++ lib/rumbl_web/param.ex
@@ -0,0 +1,7 @@
+defimpl Phoenix.Param, for: Rumbl.Multimedia.Video do
+  def to_param(%{slug: slug, id: id}) do
+    "#{id}-#{slug}"
+  end
+end
+
+# impleting the Phoenix.Param protocol for the RUmbl.Multimedia.Video struct
